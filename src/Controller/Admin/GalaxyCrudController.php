6313c21a2235737ea3209266032d9fcf06cc6d17
JoanaSolaki <joana.rodrigues.simplon@gmail.com>
2024-04-29 10:25:29 +0200

    Fix Carousel

--- src/Controller/Admin/GalaxyCrudController.php
+++ src/Controller/Admin/GalaxyCrudController.php
@@ -3,27 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Galaxy;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
-class GalaxyCrudController extends AbstractCrudController
+class GalaxyCrudController extends AbstractCrudController implements EventSubscriberInterface
 {
     public static function getEntityFqcn(): string
     {
         return Galaxy::class;
     }
 
-    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        $entityInstance->setUpdatedAt(new \DateTime());
+    // public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    // {
+    //     $entityInstance->setUpdatedAt(new \DateTime());
 
-        parent::updateEntity($entityManager, $entityInstance);
-    }
+    //     parent::updateEntity($entityManager, $entityInstance);
+    // }
 
     public function configureFields(string $pageName): iterable
     {
@@ -41,5 +45,29 @@ class GalaxyCrudController extends AbstractCrudController
                 ->setUploadDir('/public')
                 ->onlyOnIndex(),
         ];
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ["beforePersist"],
+            BeforeEntityUpdatedEvent::class => ["beforeUpdate"]
+        ];
+    }
+
+    public function beforePersist (BeforeEntityPersistedEvent $event) {
+        $entity = $event->getEntityInstance();
+
+        if ($entity instanceof Galaxy) {
+            $entity->setCreatedAt(new DateTime());
+        }
+    }
+
+    public function beforeUpdate (BeforeEntityUpdatedEvent $event) {
+        $entity = $event->getEntityInstance();
+
+        if ($entity instanceof Galaxy) {
+            $entity->setUpdatedAt(new DateTime());
+        }
     }
 }
